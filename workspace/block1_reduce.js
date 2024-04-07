a4d3bfe848bd82e3c867f4a37f19aebd754ef4bb
Jordi Bericat <jbericat@uoc.edu>
2024-04-07 21:47:46 +0000

    feat: bloc 1 - segon intent

--- workspace/block1_reduce.js
+++ workspace/block1_reduce.js
@@ -1,21 +1,18 @@
 function(key, values) {
 
-  printjson({"reduceKey": key, "values Matrix": values});
+  // printjson({"reduceKey": key, "values Array": clean_values});
 
-  let biomass_values = [];
-  let area_km2_values = [];
+  const biomass_values = values.map(item => item.biomass_value);
+  const area_km2_values = values.map(item => item.area_km2_value);
 
-  values.forEach(row => {
-    biomass_values.push(row[0]);
-    area_km2_values.push(row[1]);
-  });
+  const biomass_clean_values = biomass_values.filter(item => Number.isFinite(item));
+  const area_km2_clean_values = area_km2_values.filter(item => Number.isFinite(item));
 
-  // printjson({"reduceKey": key,
-  //            "biomass_values": biomass_values,
-  //            "area_km2_values": area_km2_values,
-  //            "biomass_sum": Array.sum(biomass_values),
-  //            "area_km2_sum": Array.sum(area_km2_values)});
+  printjson({"reduceKey": key,
+             "biomass_values": biomass_clean_values,
+             "area_km2_values": area_km2_clean_values,
+             "biomass_sum": Array.sum(biomass_clean_values),
+             "area_km2_sum": Array.sum(area_km2_clean_values)});
 
-  return Array.sum(biomass_values)/Array.sum(area_km2_values);
-  //return(values)
+  return Array.sum(biomass_clean_values)/Array.sum(area_km2_clean_values);
 }
