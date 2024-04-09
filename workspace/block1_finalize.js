0a6f1d9e3547c165d08aff959b98cdc38588c72a
Jordi Bericat <jbericat@uoc.edu>
2024-04-09 18:46:20 +0000

    feat: bloc 1 - solucio

--- workspace/block1_finalize.js
+++ workspace/block1_finalize.js
@@ -1,6 +1,5 @@
 function(key, value) {
-  //printjson({"reduceKey": key, "Value": value})
   result = value.biomass / value.area_km2
-  printjson({ "reduceKey": key, "value": result })
+  // printjson({ "reduceKey": key, "value": result })
   return result
 }
