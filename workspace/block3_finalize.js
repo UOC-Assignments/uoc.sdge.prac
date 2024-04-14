374aaee4d8bb522e8a6644d04b5efd3d2ff2563e
Jordi Bericat <jordi@beri.cat>
2024-04-14 14:28:51 +0000

    feat: block3 - v3

--- workspace/block3_finalize.js
+++ workspace/block3_finalize.js
@@ -1,39 +1,24 @@
 function(key, value) {
 
-  // Convertim el diccionari amb els valors obtinguts de reduce en una
-  // matriu per a poder iterar els elements. Aixi podrem comparar-los
-  // i obtenir la especie amb major valor de biomassa (max_biomass)
-  // INFO:
-  //  - https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript
-
-  var num_species = Object.keys(value).length;
-  var matrix = new Array(num_species).fill(null).map(() => new Array(2));
-  var row = 0;
-
-  for (specie in value) {
-    matrix[row][0] = specie;
-    matrix[row][1] = value[specie];
-    row++;
-  }
-
-  // Declarem les variables on desarem el valor maxim trobat mentre
-  // que iterem sobre la matriu, aixi com d'altres auxiliars (row)
+  // Declarem les variables on desarem el valor maxim trobat i el nom
+  // cientific de la especie
 
   var max_biomass = 0;
   var specie_with_max_biomass = '';
 
-  // Finalment iterem sobre la matriu per a trobar la especie amb valor
+  // Finalment iterem sobre el diccionari per a trobar la especie amb valor
   // maxim de biomassa, que es guardara a la variable "specie_with_max_biomass"
   // per a ser retornada per la funcio finalize
 
-  for (val of matrix) {
-    if (val[1] > max_biomass) {
-        max_biomass = val[1];
-        specie_with_max_biomass = val[0];
+  Object.keys(value).forEach(specie => {
+    var total_biomass = value[specie];
+    if (total_biomass > max_biomass) {
+      max_biomass = total_biomass;
+      specie_with_max_biomass = specie;
     }
-  }
+  });
 
-  // printjson({ "reduceKey": key, "value": specie_with_max_biomass })
+  printjson({ "reduceKey": key, "value": specie_with_max_biomass })
 
   return specie_with_max_biomass
 }
--- workspace/block3_reduce.js
+++ workspace/block3_reduce.js
@@ -5,21 +5,25 @@ function(key, values) {
   // a les conseguents invocacions de la funcio reduce per a seguir acumulant
   // la resta de subconjunts de valors associats a la mateixa clau (this.campaign)
   // INFO:
+  //  - https://www.freecodecamp.org/news/how-to-use-javascript-array-reduce-method/
   //  - https://stackoverflow.com/questions/49794240/in-javascript-array-reduce-what-does-the-accumulator-and-value-actually-mean
   //  - https://stackoverflow.com/questions/10654992/how-can-i-get-a-collection-of-keys-in-a-javascript-dictionary
 
   var biomass_by_specie = values.reduce((acc, dictionary) => {
     Object.keys(dictionary).forEach(key => {
         var value = dictionary[key];
-        acc[key] = (acc[key] || 0) + value; // Bugfix: He hagut d'implementar un workaround per a obviar algunes entrades del diccionari de valors malformades. Exemple -> "Nezumia aequalis" : 241 .......... 22.08
+        // BUGFIX: He hagut d'implementar un workaround per a obviar algunes entrades
+        // del diccionari de valors malformades (no he arribat a esbrinar el motiu del
+        // perque em passa aixo). Exemple -> "Nezumia aequalis" : 241 .......... 22.08
+        acc[key] = (acc[key] || 0) + value;
     });
     return acc;
   }, {});
 
-  printjson({
-    "reduceKey": key,
-    "biomass_by_specie": biomass_by_specie,
-  });
+  // printjson({
+  //   "reduceKey": key,
+  //   "biomass_by_specie": biomass_by_specie,
+  // });
 
   return (biomass_by_specie);
 }
